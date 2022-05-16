428435fcdbefacfc01728a1b65c829f99ea4977f
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-05-16 13:07:46 +0000

    making_Book index view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,23 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
   end
 
   def create
-
+    @book = Book.new(book_params)
   end
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
+
+
+
+
