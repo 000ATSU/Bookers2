72ad43f95a8ce9bdc3751562bf91e81813b7b849
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-05-18 13:19:21 +0000

    Cannot Log in

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,9 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
 private
