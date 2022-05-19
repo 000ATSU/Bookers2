8eea78be673a340bf465218e76ee02f158a72e13
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-05-19 13:35:02 +0000

    from index to show link

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,13 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
   def index
     @books = Book.all
   end
@@ -10,12 +17,6 @@ class BooksController < ApplicationController
   def show
   end
 
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
-  end
 
 private
 
