c82bd61fd592100ce2f53efe4364035864de90b4
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-05-24 03:36:48 +0000

    can login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'homes#top'
+  devise_for :users
   get '/homes/about' => 'homes#about', as: 'about'
   resources :books, only:[:new, :create, :index, :show]
   resources :users, only:[:index, :show, :edit, :update]
