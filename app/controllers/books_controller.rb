5e89a5ccc5fe77beae630e19196663aa9a160b23
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-05-25 08:51:02 +0000

    making edit btn

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,20 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new(book_params)
-  end
-
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book.id), notice: 'You have created book successfully.'
     else
-      redirect_to books_path
+      @user = User.find(current_user.id)
+      @books = Book.all
+      render :index
     end
   end
 
   def index
     @books = Book.all
     @user = User.find(current_user.id)
+    @book = Book.new
   end
 
   def show
